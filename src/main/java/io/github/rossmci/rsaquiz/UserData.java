2bad5bdd9f8832a5ffdc7239ea62b72e204b761f
Ross Mcinerney <rossmci@users.noreply.github.com>
2020-10-21 15:40:36 +0100

    my attempt at abstract methods

--- src/main/java/io/github/rossmci/rsaquiz/UserData.java
+++ src/main/java/io/github/rossmci/rsaquiz/UserData.java
@@ -12,11 +12,57 @@ public class UserData
 	int[] quizQuestion = new int[testSize];
 	boolean[] quizQuestionResult = new boolean[testSize];// fill loop all false to begin
 
-	void startQuiz()
+	public Object getUser()
 	{
+		return user;
+	}
+
+	public void setUser(Object user)
+	{
+		this.user = user;
+	}
+
+	public int getTestSize()
+	{
+		return testSize;
+	}
+
+	public void setTestSize(int testSize)
+	{
+		this.testSize = testSize;
+	}
+
+	public int[] getQuizQuestion()
+	{
+		return quizQuestion;
+	}
+
+	public void setQuizQuestion(int[] quizQuestion)
+	{
+		this.quizQuestion = quizQuestion;
+	}
+
+	public boolean[] getQuizQuestionResult()
+	{
+		return quizQuestionResult;
+	}
+
+	public void setQuizQuestionResult(boolean[] quizQuestionResult)
+	{
+		this.quizQuestionResult = quizQuestionResult;
+	}
+	
+	public void startQuiz()
+	{
+		for (int i = 0; i < quizQuestionResult.length; i++)
+		{
+			quizQuestionResult[i]=false;
+		}
 		//creat an array
 		//fill with for loop 0 to getRsaSignQuestion.size-1(offset by1)
 		//shuffle - link to studenbt quiz link repo
 		// filled with first 20of above array;
+		
+		// fill the buttons in a random order
 	}
 }
