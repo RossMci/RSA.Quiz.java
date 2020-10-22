e446cf5f195ce7cab676c3a8adecbe0e67325748
Ross Mcinerney <rossmci@users.noreply.github.com>
2020-10-22 20:25:19 +0100

    Quiz Building start

---
+++ RSA.Quiz/src/main/java/io/github/rossmci/rsaquiz/RsaQuizBuilder.java
@@ -0,0 +1,23 @@
+package io.github.rossmci.rsaquiz;
+
+import java.util.List;
+
+/**
+ *
+ * @author Ross Mcinerney
+ */
+public class RsaQuizBuilder
+{
+
+	List<RsaSignQuestion> buildQuizBank()
+	{
+		//TODO:  Build questions with wrong answers
+		return null;
+	}
+
+	List<RsaSignQuestion> buildQuiz()
+	{
+		return null;
+//TODO:  Build N questions for a user
+	}
+}
